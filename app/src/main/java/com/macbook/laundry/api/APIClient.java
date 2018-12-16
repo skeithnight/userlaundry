2ec3689935fd53ee492e1827763300d5a5a49d58
dwiki nugraha <skeithnight@gmail.com>
2018-12-16 19:44:39 +0700

    commit

--- app/src/main/java/com/macbook/laundry/api/APIClient.java
+++ app/src/main/java/com/macbook/laundry/api/APIClient.java
@@ -1,16 +1,17 @@
 package com.macbook.laundry.api;
 
+import com.macbook.laundry.secret.SecretData;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class APIClient {
-    public static final String BASE_URL = "http://35.237.12.254:8080/";
-    
+
+
     private static Retrofit retrofit = null;
     public static Retrofit getClient() {
         if (retrofit==null) {
             retrofit = new Retrofit.Builder()
-                    .baseUrl(BASE_URL)
+                    .baseUrl(SecretData.BASE_URL)
                     .addConverterFactory(GsonConverterFactory.create())
                     .build();
         }
